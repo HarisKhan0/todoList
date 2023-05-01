357c89ce6e558ca1c7f7637028f54d130a85d36c
Sam Todd <77017616+SamuelTodd@users.noreply.github.com>
2023-05-01 09:35:46 -0700

    added backend files

---
+++ models/user-services.js
@@ -0,0 +1,71 @@
+const mongoose = require("mongoose");
+const userModel = require("./user");
+mongoose.set("debug", true);
+
+mongoose
+  .connect("mongodb://127.0.0.1/users", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch((error) => console.log(error));
+
+async function getUsers(name, job) {
+  let result;
+  if (name === undefined && job === undefined) {
+    result = await userModel.find();
+  } else if (name && !job) {
+    result = await findUserByName(name);
+  } else if (job && !name) {
+    result = await findUserByJob(job);
+  }
+  else{
+    result = await findUserByNameAndJob(name, job);
+  }
+  return result;
+}
+
+async function findUserById(id) {
+  try {
+    return await userModel.findById(id);
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
+}
+
+async function findUserByName(name) {
+  return await userModel.find({ name: name });
+}
+
+async function findUserByJob(job) {
+  return await userModel.find({ job: job });
+}
+
+async function findUserByNameAndJob(name, job) {
+  return await userModel.find({ name: name, job: job });
+}
+
+async function addUser(user) {
+  try {
+    const userToAdd = new userModel(user);
+    const savedUser = await userToAdd.save();
+    return savedUser;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
+
+async function deleteUserById(id) {
+  try {
+    return await userModel.findByIdAndDelete(id);
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
+}
+
+exports.getUsers = getUsers;
+exports.findUserById = findUserById;
+exports.addUser = addUser;
+exports.deleteUserById = deleteUserById;
