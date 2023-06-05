e27709af2e5ecd8cdbdf9a05f9361cf004f6a3c2
HarisKhan0 <71156152+HarisKhan0@users.noreply.github.com>
2023-06-05 09:48:18 -0700

    Code complete

--- frontend/src/wview.js
+++ frontend/src/wview.js
@@ -3,8 +3,8 @@ import React from "react";
 const calendarStyle = {
   fontFamily: "Arial, sans-serif",
   margin: "0 auto", // Center the calendar horizontally
-  height: "90vh", // Set height to 100% of the viewport height
-  width: "90vw", // Set width to 100% of the viewport width
+  height: "90vh", // Set height to 90% of the viewport height
+  width: "90vw", // Set width to 90% of the viewport width
 };
 
 const headerStyle = {
@@ -40,94 +40,74 @@ const dayTextStyle = {
 const taskStyle = {
   marginLeft: "0",
   padding: "10px",
-  //   borderRadius: "4px",
-  //   marginBottom: "4px", // Add a little distance between each task
+  borderRadius: "4px",
+  marginBottom: "4px", // Add a little distance between each task
   textAlign: "left", // Align tasks to the left
   width: "100%", // Make tasks stretch to the width of the box
-  //   boxSizing: "border-box", // Include padding within the width
-  //   display: "flex", // Use flexbox to align items
-  justifyContent: "start", // Align items to the left
+  boxSizing: "border-box", // Include padding within the width
 };
 
-class WeeklyViewCalendar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentWeek: new Date(),
-      tasks: [
-        { date: "2023-06-05", tasks: ["Task 1", "Task 2"] },
-        { date: "2023-06-06", tasks: ["Task 3"] },
-        { date: "2023-06-07", tasks: [] },
-        { date: "2023-06-08", tasks: ["Task 4"] },
-        { date: "2023-06-09", tasks: [] },
-        { date: "2023-06-10", tasks: [] },
-        { date: "2023-06-11", tasks: [] },
-      ],
-    };
-  }
+const WeeklyViewCalendar = ({ taskData }) => {
+  const [currentWeek, setCurrentWeek] = React.useState(new Date());
 
-  prevWeek = () => {
-    const { currentWeek } = this.state;
+  const prevWeek = () => {
     const prevWeek = new Date(currentWeek);
     prevWeek.setDate(prevWeek.getDate() - 7);
-    this.setState({ currentWeek: prevWeek });
+    setCurrentWeek(prevWeek);
   };
 
-  nextWeek = () => {
-    const { currentWeek } = this.state;
+  const nextWeek = () => {
     const nextWeek = new Date(currentWeek);
     nextWeek.setDate(nextWeek.getDate() + 7);
-    this.setState({ currentWeek: nextWeek });
+    setCurrentWeek(nextWeek);
   };
 
-  render() {
-    const { currentWeek, tasks } = this.state;
-    const weekDays = [];
-    const weekStart = new Date(currentWeek);
-    weekStart.setDate(weekStart.getDate() - weekStart.getDay());
-    const weekEnd = new Date(weekStart);
-    weekEnd.setDate(weekEnd.getDate() + 6);
+  const weekDays = [];
+  const weekStart = new Date(currentWeek);
+  weekStart.setDate(weekStart.getDate() - weekStart.getDay());
+  const weekEnd = new Date(weekStart);
+  weekEnd.setDate(weekEnd.getDate() + 6);
 
-    for (let i = 0; i < 7; i++) {
-      const currentDate = new Date(weekStart);
-      currentDate.setDate(currentDate.getDate() + i);
-      weekDays.push(currentDate);
-    }
-
-    return (
-      <div style={calendarStyle}>
-        <div style={headerStyle}>
-          <button onClick={this.prevWeek}>Previous Week</button>
-          <h1>
-            Week of {weekStart.toDateString()} - {weekEnd.toDateString()}
-          </h1>
-          <button onClick={this.nextWeek}>Next Week</button>
-        </div>
-        <ul style={weekStyle}>
-          {weekDays.map((day) => {
-            const dayTasks = tasks.find(
-              (task) => task.date === day.toISOString().slice(0, 10)
-            );
-            return (
-              <li key={day.toDateString()} style={dayStyle}>
-                <strong style={dayTextStyle}>
-                  {day.toLocaleDateString(undefined, { weekday: "long" })}
-                </strong>
-                <ul>
-                  {dayTasks &&
-                    dayTasks.tasks.map((task, index) => (
-                      <li key={index} style={taskStyle}>
-                        {task}
-                      </li>
-                    ))}
-                </ul>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    );
+  for (let i = 0; i < 7; i++) {
+    const currentDate = new Date(weekStart);
+    currentDate.setDate(currentDate.getDate() + i);
+    weekDays.push(currentDate);
   }
-}
+
+  return (
+    <div style={calendarStyle}>
+      <div style={headerStyle}>
+        <button onClick={prevWeek}>Previous Week</button>
+        <h1>
+          Week of {weekStart.toDateString()} - {weekEnd.toDateString()}
+        </h1>
+        <button onClick={nextWeek}>Next Week</button>
+      </div>
+      <ul style={weekStyle}>
+        {weekDays.map((day) => {
+          const dayTasks = taskData.filter(
+            (task) =>
+              new Date(task.days).toISOString().slice(0, 10) ===
+              day.toISOString().slice(0, 10)
+          );
+          return (
+            <li key={day.toDateString()} style={dayStyle}>
+              <strong style={dayTextStyle}>
+                {day.toLocaleDateString(undefined, { weekday: "long" })}
+              </strong>
+              <ul>
+                {dayTasks.map((task, index) => (
+                  <li key={index} style={taskStyle}>
+                    <strong>{task.task_name}</strong>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
 export default WeeklyViewCalendar;
